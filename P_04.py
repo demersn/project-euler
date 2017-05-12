d0be2c0ef53d6466ba4fa290e57defcbae24ff13
Nicolas Demers <nd.nicolasdemers@gmail.com>
2017-05-11 23:12:25 -0400

    flake8 for old problems

--- P_04.py
+++ P_04.py
@@ -1,21 +1,25 @@
 # Find the largest palindrome made from the product of two 3-digit numbers
 
+
 # We start by creating a function that checks if a number is a palindrome
 def is_pal(x):
-    string=str(x)
+    string = str(x)
     for z in range(len(string)):
-        if string[z]!=string[-(z+1)]:
+        if string[z] != string[-(z+1)]:
             return False
             continue
 
+
 # We use it for the desired range of numbers
 def problem4():
-    for x in range(999,99,-1):
-        for y in range(999,99,-1):
-            if is_pal(x*y) == None:
+    for x in range(999, 99, -1):
+        for y in range(999, 99, -1):
+            if is_pal(x*y) is None:
                 return(x*y)
                 break
 
+
 print(problem4())
 
-# This version of the code has trouble exiting the palindrome chek in a way that is clean and easy for the other function to use.
+# This version of the code has trouble exiting the palindrome chek in a way
+# that is clean and easy for the other function to use.
